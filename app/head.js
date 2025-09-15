03cb8c3cc32b1261a73c750e4e982ecffeb4a321
Sully Lawrence <sullylawrence@Mac.attlocal.net>
2025-09-15 15:29:30 -0500

    Add custom page title and meta

--- app/head.js
+++ app/head.js
@@ -2,7 +2,7 @@ export default function Head() {
   return (
     <>
       <title>Sully & Chandler - Save the Date</title>
-      <meta name="description" content="Sully and Chandler are getting married! Save the Date!" />
+      <meta name="description" content="Save the Date!" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
     </>
